3d5217f6f38a0d1f201dbc3421ab4953f8de584e
Shubham <49046719+shubhs10@users.noreply.github.com>
2023-04-10 14:09:38 +0530

    Update the changes

--- functions/server.js
+++ functions/server.js
@@ -3,10 +3,8 @@ const express =  require("express")
 const axios = require("axios")
 const cors = require('cors')
 const { response } = require("express")
-const serverless = require("serverless-http")
 
 const app =  express()
-
 const port = process.env.APP_PORT || 5000
 app.use(cors())
 
@@ -23,5 +21,3 @@ app.get('/mySpaceXData',async(req,res)=>{
 app.listen(port,()=>{
   console.log("Server up and running at:",port)
 })
-
-module.exports.handler = serverless(app)
